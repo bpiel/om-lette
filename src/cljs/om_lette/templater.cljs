8d58ae4c72c9da70d0c3d20d906b1863576da692
Bill Piel <bill@billpiel.com>
2014-12-11 14:25:54 -0500

    basic template->hiccup. Only resolves :om-code

--- src/cljs/om_lette/templater.cljs
+++ src/cljs/om_lette/templater.cljs
@@ -13,11 +13,6 @@
     (if (has-all-keys? @template-cache names)
       (callback))))
 
-(defn make-template-handler [template-name callback]
-  (fn handler [response]
-    (swap! template-cache assoc template-name response)
-    (callback)))
-
 ;; regex
 (def nonnum "[a-zA-Z\\*\\+!\\-\\_\\?]")
 (def valid-sym-chars "[\\w\\d\\*\\+!\\-\\_\\?]")
@@ -58,6 +53,20 @@
                not)
           x))
 
+(defn flatten-out
+  [t]
+  (if (like-html-vec? t)
+    (into (->> t (take 2) vec)
+          (mapcat #(if (and (sequential? %)
+                            (-> % like-html-vec? not))
+                     %
+                     [%])
+                  (drop 2 t)))
+    t))
+
+#_(flatten-out [:div {} "a" [:div {}] '("whoa" [:a ""])])
+
+
 (defn split-dbl-brackets
   [t]
   (-> t
@@ -95,7 +104,8 @@
        hick/as-hiccup
        (walk/postwalk (fn [t] (cond (string? t) (tokenize-string t)
                                     (like-html-vec? t) (update-in t [1] grab-om-attrs [:om-if :om-repeat])
-                                    :else t)))))
+                                    :else t)))
+       (walk/postwalk flatten-out)))
 
 (defn load-templates [names get-html-template callback]
   (mapv #(->> (fn [response]
@@ -107,9 +117,18 @@
               (get-html-template %))
         names))
 
+(defn template->hiccup
+  [tmplt state]
+  (walk/prewalk (fn [x]
+                  (cond (and (vector? x)
+                             (= (first x) :om-code)) (get state (second x) "")
+                        :else x))
+                tmplt))
 
+#_ (-> "<div>{{a}}</div" html->template (template->hiccup {"a" 33}))
 
-#_(html->template "<div om-repeat='{{a}}'></div>")
+#_(html->template "<div>a <span></span></div>")
+
 
 #_(load-templates ["hello.html"]
                   (fn [n h] (println "in get-html-template") (h "<div></div"))
